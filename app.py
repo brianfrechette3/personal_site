a074690a0a624004072ef992a7b5d385972544db
brianfrechette3 <bfrec@seas.upenn.edu>
2024-08-04 19:22:00 -0400

    added experience section, next will be to add hobbies to about with carousel

--- app.py
+++ app.py
@@ -7,13 +7,19 @@ app = Flask(__name__)
 def about():
     return render_template("index.html", title="Brian Frechette - About")
 
-@app.route("/brian_ai")
-def brian_ai():
-    return render_template("brian_ai.html", title="Brian.AI - ChatBot")
+@app.route("/experience")
+def experience():
+    return render_template("experience.html", title="Brian Frechette - Experience")
 
 @app.route("/projects")
 def projects():
     return render_template("projects.html", title="Brian Frechette - Projects")
+
+@app.route("/brian_ai")
+def brian_ai():
+    return render_template("brian_ai.html", title="Brian.AI - ChatBot")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/index.js
+++ static/index.js
@@ -7,7 +7,7 @@ var typewriter = new Typewriter(app, {
 
 typewriter
   .pauseFor(1000)
-  .typeString('I\'m an engineer interested in software, data, and artificial intelligence.')
+  .typeString('I\'m an engineer interested in software, data, and artificial intelligence. Scroll down to learn more.')
   .pauseFor(3000)
   .start()
 
@@ -33,121 +33,175 @@ var granimInstance = new Granim({
     }
 });
 
-//BUTTON 1
-var modal1 = document.getElementById("modal1");
+// //BUTTON 1 - MSE-AI
+// var modal1 = document.getElementById("modal1");
 
-// Get the button that opens the modal
-var btnModal1 = document.getElementById("btn1");
+// // Get the button that opens the modal
+// var btnModal1 = document.getElementById("btn1");
 
-// Get the <span> element that closes the modal
-var spanModal1 = document.getElementsByClassName("closeModal1")[0];
+// // Get the <span> element that closes the modal
+// var spanModal1 = document.getElementsByClassName("closeModal1")[0];
 
-// When the user clicks on the button, open the modal
-btnModal1.onclick = function() {
-  modal1.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btnModal1.onclick = function() {
+//   modal1.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-spanModal1.onclick = function() {
-  modal1.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// spanModal1.onclick = function() {
+//   modal1.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal1) {
-    modal1.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal1) {
+//     modal1.style.display = "none";
+//   }
+// }
 
-//BUTTON 2
-var modal2 = document.getElementById("modal2");
+// //BUTTON 2 - MCIT
+// var modal2 = document.getElementById("modal2");
 
-// Get the button that opens the modal
-var btnModal2 = document.getElementById("btn2");
+// // Get the button that opens the modal
+// var btnModal2 = document.getElementById("btn2");
 
-// Get the <span> element that closes the modal
-var spanModal2 = document.getElementsByClassName("closeModal2")[0];
+// // Get the <span> element that closes the modal
+// var spanModal2 = document.getElementsByClassName("closeModal2")[0];
 
-// When the user clicks on the button, open the modal
-btnModal2.onclick = function() {
-  modal2.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btnModal2.onclick = function() {
+//   console.log("made it here 2");
+//   modal2.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-spanModal2.onclick = function() {
-  modal2.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// spanModal2.onclick = function() {
+//   modal2.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal2) {
+//     modal2.style.display = "none";
+//   }
+// }
 
-//BUTTON 3 -- links to outside website
-var btn3 = document.getElementById("btn3");
-btn3.onclick = function() { 
-  window.open("https://www.nintendo.com", "_blank") //TODO add your link
-}
+// // //BUTTON 3 -- links to outside website
+// // var btn3 = document.getElementById("btn3");
+// // btn3.onclick = function() { 
+// //   window.open("https://www.nintendo.com", "_blank") //TODO add your link
+// // }
 
-//BUTTON 4
-var modal4 = document.getElementById("modal4");
+// //BUTTON 4 - UConn ME
+// var modal4 = document.getElementById("modal4");
 
-// Get the button that opens the modal
-var btnModal4 = document.getElementById("btn4");
+// // Get the button that opens the modal
+// var btnModal4 = document.getElementById("btn4");
 
-// Get the <span> element that closes the modal
-var spanModal4 = document.getElementsByClassName("closeModal4")[0];
+// // Get the <span> element that closes the modal
+// var spanModal4 = document.getElementsByClassName("closeModal4")[0];
 
-// When the user clicks on the button, open the modal
-btnModal4.onclick = function() {
-  modal4.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btnModal4.onclick = function() {
+//   console.log("made it here 4");
+//   modal4.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-spanModal4.onclick = function() {
-  modal4.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// spanModal4.onclick = function() {
+//   modal4.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal4) {
-    modal4.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal4) {
+//     modal4.style.display = "none";
+//   }
+// }
 
-//BUTTON 5
-var modal5 = document.getElementById("modal5");
+// //BUTTON 5 - PW
+// var modal5 = document.getElementById("modal5");
 
-// Get the button that opens the modal
-var btnModal5 = document.getElementById("btn5");
+// // Get the button that opens the modal
+// var btnModal5 = document.getElementById("btn5");
 
-// Get the <span> element that closes the modal
-var spanModal5 = document.getElementsByClassName("closeModal5")[0];
+// // Get the <span> element that closes the modal
+// var spanModal5 = document.getElementsByClassName("closeModal5")[0];
 
-// When the user clicks on the button, open the modal
-btnModal5.onclick = function() {
-  modal5.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btnModal5.onclick = function() {
+//   modal5.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-spanModal5.onclick = function() {
-  modal5.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// spanModal5.onclick = function() {
+//   modal5.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal5) {
-    modal5.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal5) {
+//     modal5.style.display = "none";
+//   }
+// }
 
-//BUTTON 6 -- links to outside website
-var btn6 = document.getElementById("btn6");
-btn6.onclick = function() { 
-  window.open("https://www.arduino.cc", "_blank") //TODO add your link
-}
+// //BUTTON 6 - TRIUMPH
+// var modal6 = document.getElementById("modal6");
+
+// // Get the button that opens the modal
+// var btnModal6 = document.getElementById("btn6");
+
+// // Get the <span> element that closes the modal
+// var spanModal6 = document.getElementsByClassName("closeModal6")[0];
+
+// // When the user clicks on the button, open the modal
+// btnModal6.onclick = function() {
+//   modal6.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// spanModal6.onclick = function() {
+//   modal6.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal6) {
+//     modal6.style.display = "none";
+//   }
+// }
+
+// //BUTTON 7 - UConn ITS
+// var modal7 = document.getElementById("modal7");
+
+// // Get the button that opens the modal
+// var btnModal7 = document.getElementById("btn7");
+
+// // Get the <span> element that closes the modal
+// var spanModal7= document.getElementsByClassName("closeModal7")[0];
+
+// // When the user clicks on the button, open the modal
+// btnModal7.onclick = function() {
+//   modal7.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// spanModal7.onclick = function() {
+//   modal7.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal7) {
+//     modal7.style.display = "none";
+//   }
+// }
+
+// //BUTTON 6 -- links to outside website
+// var btn6 = document.getElementById("btn6");
+// btn6.onclick = function() { 
+//   window.open("https://www.arduino.cc", "_blank") //TODO add your link
+// }
 
 //HOBBY MODALS
 
